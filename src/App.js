5879dbb4f050659646a9e0e388977345ee7d1e08
chandlerblaschka <cblaschka@live.com>
2022-04-29 09:39:13 -0500

    working form and sorting fetched data by industry

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -13,22 +13,45 @@ import Landscape from "./components/Landscape";
 import SkyHarvester from "./components/SkyHarvester";
 import Muni from "./components/Muni";
 import NavBar from "./components/NavBar";
-
+import axios from "axios";
 
 function App() {
+
+  let [loading, setLoading] = useState(true)
+  let [data, setData] = useState({})
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const { data: response } = await axios.get('http://localhost:8000/golf/')
+        setData(response)
+      } catch (error) {
+        console.error(error.message)
+      }
+      setLoading(false);
+    }
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
-      <NavBar />
-      <Fragment>
-        <Routes>
-          <Route exact path="/Golf" element={<Golf />} />
-          <Route exact path="/Landscape" element={<Landscape />} />
-          <Route exact path="/Muni" element={<Muni />} />
-          <Route exact path="/Ag" element={<Ag />} />
-          <Route exact path="/SkyHarvester" element={<SkyHarvester />} />
-          <Route exact path="/" element={<Dashboard />} />
-        </Routes >
-      </Fragment>
+      {loading && <div>Loading</div>}
+      {!loading && (
+        <div>
+          <NavBar />
+          <Fragment>
+            <Routes>
+              <Route exact path="/Golf" element={<Golf data={data} />} />
+              <Route exact path="/Landscape" element={<Landscape />} />
+              <Route exact path="/Muni" element={<Muni />} />
+              <Route exact path="/Ag" element={<Ag />} />
+              <Route exact path="/SkyHarvester" element={<SkyHarvester />} />
+              <Route exact path="/" element={<Dashboard />} />
+            </Routes >
+          </Fragment>
+        </div>
+      )}
     </div >
   );
 }
--- src/components/Golf.js
+++ src/components/Golf.js
@@ -1,13 +1,40 @@
-import react, { useState } from "react";
+import react, { useEffect, useState } from "react";
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
 import Container from "react-bootstrap/Container";
 import Table from 'react-bootstrap/Table'
 import axios from "axios";
 
-function Golf() {
+function Golf(props) {
 
-    let [industry, setIndustry] = useState("")
+    console.log(props.data)
+    let actions = Array.from(props.data)
+    let actionList = []
+
+    if (actions) {
+        actionList = actions.map((action) => {
+            return <tr key={action.id}>
+                <td>{action.oppNumber}</td>
+                <td>{action.name}</td>
+                <td>{action.prodCode}</td>
+                <td>{action.request}</td>
+                <td>{action.sales}</td>
+                <td>{action.projManager}</td>
+                <td>{action.engineer}</td>
+                <td>{action.reqDate}</td>
+                <td>{action.dueDate}</td>
+                <td>{action.compDate}</td>
+                <td>{action.comments}</td>
+                <td>
+                    <Button size="sm" type="submit"  >Edit</Button>
+                </td>
+            </tr>
+        })
+    }
+
+
+
+    // let [industry, setIndustry] = useState("")
     let [oppNumber, setOppNumber] = useState("")
     let [name, setName] = useState("")
     let [prodCode, setProdCode] = useState("")
@@ -24,7 +51,7 @@ function Golf() {
         e.preventDefault();
 
         axios.post('http://localhost:8000/golf/', {
-            industry: industry,
+            industry: "Golf",
             oppNumber: oppNumber,
             name: name,
             prodCode: prodCode,
@@ -40,6 +67,7 @@ function Golf() {
             .then(res => {
                 console.log(res);
                 console.log(res.data)
+                window.location.reload()
             })
             .catch(error => {
                 console.log(error.response)
@@ -67,201 +95,162 @@ function Golf() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>002435</td>
-                        <td>Johnathan Landing</td>
-                        <td>VT</td>
-                        <td>CSO Dwg</td>
-                        <td>Green</td>
-                        <td>DP</td>
-                        <td>DP</td>
-                        <td>2/4/2022</td>
-                        <td>2/11/2022</td>
-                        <td></td>
-                        <td>None</td>
-                        <td>
-                            <Button size="sm" type="submit"  >Edit</Button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>004531</td>
-                        <td>Squires Course</td>
-                        <td>VTVQ</td>
-                        <td>CSO Dwg</td>
-                        <td>JVC</td>
-                        <td>MJH</td>
-                        <td>MJH</td>
-                        <td>2/5/2022</td>
-                        <td>2/12/2022</td>
-                        <td></td>
-                        <td>None</td>
-                        <td>
-                            <Button size="sm" type="submit"  >Edit</Button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>002541</td>
-                        <td>Peter's Hill</td>
-                        <td>HC</td>
-                        <td>Release Mtg</td>
-                        <td>Salisbury</td>
-                        <td>DP</td>
-                        <td>MTM</td>
-                        <td>2/6/2022</td>
-                        <td>2/8/2022</td>
-                        <td></td>
-                        <td>None</td>
-                        <td>
-                            <Button size="sm" type="submit"  >Edit</Button>
-                        </td>
-                    </tr>
+                    {actionList}
                 </tbody>
             </Table>
             <br></br>
             <br></br>
-            <Table striped bordered hover size="sm" style={{ width: "95%", margin: "auto" }}>
-                <thead>
-                    <tr>
-                        <th>Opportunity Number</th>
-                        <th>Project Name</th>
-                        <th>Product Code</th>
-                        <th>Request</th>
-                        <th>Salesman</th>
-                        <th>Project Manager</th>
-                        <th>Engineer</th>
-                        <th>Request Date</th>
-                        <th>Due Date</th>
-                        <th>Date Completed</th>
-                        <th>Comments</th>
-                        <th>Add</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                            <Form.Control size="sm" type="number" onChange={(e) => setIndustry(e.target.value)} />
-                            <Form.Text >
-                            </Form.Text>
-                        </td>
-                        <td>
-                            <Form.Control size="sm" type="text" onChange={(e) => setName(e.target.value)} />
-                            <Form.Text >
-                            </Form.Text>
-                        </td>
-                        <td>
-                            <Form.Select size="sm" onChange={(e) => setProdCode(e.target.value)} >
-                                <option>AGCP</option>
-                                <option>BMX</option>
-                                <option>CLT</option>
-                                <option>CP</option>
-                                <option>HC</option>
-                                <option>LST</option>
-                                <option>MB</option>
-                                <option>MO</option>
-                                <option>RET</option>
-                                <option>RLT</option>
-                                <option>S3</option>
-                                <option>SH</option>
-                                <option>SST</option>
-                                <option>ST</option>
-                                <option>STP</option>
-                                <option>VC</option>
-                                <option>VMS</option>
-                                <option>VT</option>
-                                <option>VTVQ</option>
-                            </Form.Select>
-                        </td>
-                        <td>
-                            <Form.Select size="sm" onChange={(e) => setRequest(e.target.value)}>
-                                <option>Book Order</option>
-                                <option>Budget $</option>
-                                <option>CSO Dwg</option>
-                                <option>Drawing</option>
-                                <option>Elec Doc's</option>
-                                <option>Other</option>
-                                <option>Quote</option>
-                                <option>Release Mtg</option>
-                                <option>Spec</option>
-                                <option>Sub/O&M</option>
-                            </Form.Select>
-                        </td>
-                        <td>
-                            <Form.Select size="sm" onChange={(e) => setSales(e.target.value)}  >
-                                <option>Salesman</option>
-                            </Form.Select>
-                        </td>
-                        <td>
-                            <Form.Select size="sm" onChange={(e) => setProjManager(e.target.value)}>
-                                <option>AJC</option>
-                                <option>BXR</option>
-                                <option>CSB</option>
-                                <option>DP</option>
-                                <option>DXT</option>
-                                <option>EE</option>
-                                <option>ESP</option>
-                                <option>ESP</option>
-                                <option>EXR</option>
-                                <option>GAG</option>
-                                <option>GDS</option>
-                                <option>GOZ</option>
-                                <option>JWN</option>
-                                <option>MJH</option>
-                                <option>MJW</option>
-                                <option>MTM</option>
-                                <option>NAV</option>
-                                <option>PJB</option>
-                                <option>RJS</option>
-                                <option>RK</option>
-                                <option>SLM</option>
-                                <option>SRS</option>
-                            </Form.Select>
-                        </td>
-                        <td>
-                            <Form.Select size="sm" onChange={(e) => setEngineer(e.target.value)}>
-                                <option>AJC</option>
-                                <option>BXR</option>
-                                <option>CSB</option>
-                                <option>DP</option>
-                                <option>DXT</option>
-                                <option>EE</option>
-                                <option>ESP</option>
-                                <option>ESP</option>
-                                <option>EXR</option>
-                                <option>GAG</option>
-                                <option>GDS</option>
-                                <option>GOZ</option>
-                                <option>JWN</option>
-                                <option>MJH</option>
-                                <option>MJW</option>
-                                <option>MTM</option>
-                                <option>NAV</option>
-                                <option>PJB</option>
-                                <option>RJS</option>
-                                <option>RK</option>
-                                <option>SLM</option>
-                                <option>SRS</option>
-                            </Form.Select>
-                        </td>
-                        <td>
-                            <Form.Control size="sm" type="date" onChange={(e) => setReqDate(e.target.value)} />
-                        </td>
-                        <td>
-                            <Form.Control size="sm" type="date" onChange={(e) => setDueDate(e.target.value)} />
-                        </td>
-                        <td>
-                            <Form.Control size="sm" type="date" onChange={(e) => setCompDate(e.target.value)} />
-                        </td>
-                        <td>
-                            <Form.Control size="sm" type="text" onChange={(e) => setComments(e.target.value)} />
-                            <Form.Text >
-                            </Form.Text>
-                        </td>
-                        <td>
-                            <Button size="sm" type="submit" onSubmit={(e) => submitPost(e)} >Add</Button>
-                        </td>
-                    </tr>
-                </tbody>
-            </Table>
+            <form onSubmit={(e) => submitPost(e)}>
+                <Table striped bordered hover size="sm" style={{ width: "95%", margin: "auto" }}>
+                    <thead>
+                        <tr>
+                            <th>Opportunity Number</th>
+                            <th>Project Name</th>
+                            <th>Product Code</th>
+                            <th>Request</th>
+                            <th>Salesman</th>
+                            <th>Project Manager</th>
+                            <th>Engineer</th>
+                            <th>Request Date</th>
+                            <th>Due Date</th>
+                            <th>Date Completed</th>
+                            <th>Comments</th>
+                            <th>Add</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>
+                                <Form.Control size="sm" type="number" onChange={(e) => setOppNumber(e.target.value)} />
+                                <Form.Text >
+                                </Form.Text>
+                            </td>
+                            <td>
+                                <Form.Control size="sm" type="text" onChange={(e) => setName(e.target.value)} />
+                                <Form.Text >
+                                </Form.Text>
+                            </td>
+                            <td>
+                                <Form.Select size="sm" onChange={(e) => setProdCode(e.target.value)} >
+                                    <option>PC</option>
+                                    <option>AGCP</option>
+                                    <option>BMX</option>
+                                    <option>CLT</option>
+                                    <option>CP</option>
+                                    <option>HC</option>
+                                    <option>LST</option>
+                                    <option>MB</option>
+                                    <option>MO</option>
+                                    <option>RET</option>
+                                    <option>RLT</option>
+                                    <option>S3</option>
+                                    <option>SH</option>
+                                    <option>SST</option>
+                                    <option>ST</option>
+                                    <option>STP</option>
+                                    <option>VC</option>
+                                    <option>VMS</option>
+                                    <option>VT</option>
+                                    <option>VTVQ</option>
+                                </Form.Select>
+                            </td>
+                            <td>
+                                <Form.Select size="sm" onChange={(e) => setRequest(e.target.value)}>
+                                    <option>Request</option>
+                                    <option>Book Order</option>
+                                    <option>Budget $</option>
+                                    <option>CSO Dwg</option>
+                                    <option>Drawing</option>
+                                    <option>Elec Doc's</option>
+                                    <option>Other</option>
+                                    <option>Quote</option>
+                                    <option>Release Mtg</option>
+                                    <option>Spec</option>
+                                    <option>Sub/O&M</option>
+                                </Form.Select>
+                            </td>
+                            <td>
+                                <Form.Select size="sm" onChange={(e) => setSales(e.target.value)}  >
+                                    <option>Salesman</option>
+                                    <option>Green</option>
+                                </Form.Select>
+                            </td>
+                            <td>
+                                <Form.Select size="sm" onChange={(e) => setProjManager(e.target.value)}>
+                                    <option>PM</option>
+                                    <option>AJC</option>
+                                    <option>BXR</option>
+                                    <option>CSB</option>
+                                    <option>DP</option>
+                                    <option>DXT</option>
+                                    <option>EE</option>
+                                    <option>ESP</option>
+                                    <option>ESP</option>
+                                    <option>EXR</option>
+                                    <option>GAG</option>
+                                    <option>GDS</option>
+                                    <option>GOZ</option>
+                                    <option>JWN</option>
+                                    <option>MJH</option>
+                                    <option>MJW</option>
+                                    <option>MTM</option>
+                                    <option>NAV</option>
+                                    <option>PJB</option>
+                                    <option>RJS</option>
+                                    <option>RK</option>
+                                    <option>SLM</option>
+                                    <option>SRS</option>
+                                </Form.Select>
+                            </td>
+                            <td>
+                                <Form.Select size="sm" onChange={(e) => setEngineer(e.target.value)}>
+                                    <option>Eng.</option>
+                                    <option>AJC</option>
+                                    <option>BXR</option>
+                                    <option>CSB</option>
+                                    <option>DP</option>
+                                    <option>DXT</option>
+                                    <option>EE</option>
+                                    <option>ESP</option>
+                                    <option>ESP</option>
+                                    <option>EXR</option>
+                                    <option>GAG</option>
+                                    <option>GDS</option>
+                                    <option>GOZ</option>
+                                    <option>JWN</option>
+                                    <option>MJH</option>
+                                    <option>MJW</option>
+                                    <option>MTM</option>
+                                    <option>NAV</option>
+                                    <option>PJB</option>
+                                    <option>RJS</option>
+                                    <option>RK</option>
+                                    <option>SLM</option>
+                                    <option>SRS</option>
+                                </Form.Select>
+                            </td>
+                            <td>
+                                <Form.Control size="sm" type="date" onChange={(e) => setReqDate(e.target.value)} />
+                            </td>
+                            <td>
+                                <Form.Control size="sm" type="date" onChange={(e) => setDueDate(e.target.value)} />
+                            </td>
+                            <td>
+                                <Form.Control size="sm" type="date" onChange={(e) => setCompDate(e.target.value)} />
+                            </td>
+                            <td>
+                                <Form.Control size="sm" type="text" onChange={(e) => setComments(e.target.value)} />
+                                <Form.Text >
+                                </Form.Text>
+                            </td>
+                            <td>
+                                <Button size="sm" type="submit"  >Add</Button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </form>
         </div>
     )
 }
